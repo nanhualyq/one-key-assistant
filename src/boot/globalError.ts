a5c61e435856313c1a1ae363d6a6819aabbb82fe
lyq <nanhualyq@gmail.com>
2025-10-17 11:54:19 +0800

    fix: ResizeObserver error

--- src/boot/globalError.ts
+++ src/boot/globalError.ts
@@ -13,6 +13,10 @@ export default defineBoot(({ app }) => {
   app.config.errorHandler = showError
   window.addEventListener('error', (event, ...rest) => {
     console.error(event, rest);
+    // q-page 第一次出现滚动条的时候会报这个错，但是gemini聊天就是会超出视图区域，只能先跳过，避免频繁的显示错误对话框
+    if (event.message === 'ResizeObserver loop completed with undelivered notifications.') {
+      return
+    }
     showError(event.error || event.message)
   })
   window.addEventListener('unhandledrejection', (event, ...rest) => {
